f09ba849b1511fd8eca06006eb35c680789eccc6
Ilya Homyakov <36329586+IlyaHomyakov@users.noreply.github.com>
2021-04-25 15:22:10 +0300

    v4.25.1 / static files linking problem solved, added hard-coded main page with full css&js. Works correctly only with 1280-1600px device width

---
+++ journal_beta/main/static/js/main.js
@@ -0,0 +1,23 @@
+// alert("HELLO");
+
+function show_side_block(){
+    let side_block = document.getElementsByClassName('side_block')[0];
+    let info = document.getElementsByClassName('info')[0];
+    let side_block_styles = getComputedStyle(side_block);
+    if (side_block_styles.left === '-500px'){
+        side_block.style.setProperty('left', '0px');
+        side_block.style.setProperty('box-shadow', 'rgba(0,0,0,0.16) 0 0 2px');
+        info.innerHTML =
+            '<span class="material-icons-outlined">\n' +
+            '    close\n' +
+            '</span>'
+    } else{
+        side_block.style.setProperty('left', '-500px');
+        side_block.style.setProperty('box-shadow', 'none');
+        info.innerHTML =
+            '<span class="material-icons-outlined">\n' +
+            '    info\n' +
+            '</span>'
+    }
+
+}
