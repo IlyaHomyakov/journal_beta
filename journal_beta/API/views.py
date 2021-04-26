64cef405723abe97dca900b5df681eeb2766fc98
Ilya Homyakov <36329586+IlyaHomyakov@users.noreply.github.com>
2021-04-26 16:56:23 +0300

    v4.26 / dynamic week changes

--- journal_beta/API/views.py
+++ journal_beta/API/views.py
@@ -1,8 +1,8 @@
 import datetime
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from .models import SemesterOptions
-from time import strftime
 
 
 def index(request):
@@ -10,7 +10,28 @@ def index(request):
 
 
 def week_type(request):
-    start_day = SemesterOptions.objects.get(semester_start__year=datetime.date.today().year)
-    print(start_day)
-    return HttpResponse(strftime("%V"))
-    # return '1'
+    semester_opts = SemesterOptions.objects.values().get()
+    start_week_type = semester_opts['server_week_type_choice']
+    semester_start_week = semester_opts['semester_start'].isocalendar()[1]
+    current_week = datetime.date.today().isocalendar()[1]
+
+    return_data = {
+        'currentWeekType': None
+    }
+    i = semester_start_week
+    if int(start_week_type) == 1:
+        tmp = int(start_week_type)
+        while i <= current_week:
+            tmp = 1 - tmp
+            print(tmp + 1, " ")
+            return_data['currentWeekType'] = tmp + 1
+            i += 1
+    else:
+        tmp = int(start_week_type)
+        while i <= current_week:
+            tmp = 1 - tmp
+            print(tmp - 1, " ")
+            return_data['currentWeekType'] = tmp - 1
+            i += 1
+
+    return JsonResponse(return_data)
--- journal_beta/main/views.py
+++ journal_beta/main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
+import requests
 
 
 def main_page(request):
     today_day = datetime.today().date().day
     today_month = datetime.today().date().month
+
+    today_week_type = requests.get('http://127.0.0.1:8000/api/weektype/').json()
+    if today_week_type['currentWeekType'] == 1:
+        today_week_type = ', нечетная неделя'
+    elif today_week_type['currentWeekType'] == 2:
+        today_week_type = ', четная неделя'
+    else:
+        today_week_type = ''
     context = {
         'today_day': today_day,
         'today_month': today_month,
+        'today_week_type': today_week_type
     }
     return render(request, 'main/index.html', context)
-
-
