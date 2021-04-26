10ea4fbeedab82c4627c85172611274cd809b749
Ilya Homyakov <36329586+IlyaHomyakov@users.noreply.github.com>
2021-04-26 20:10:54 +0300

    v4.26 / dynamic week changes, timezone set to Europe/Minsk, added venv for servers, removed some files, added mobile version for main page

--- journal_beta/main/static/js/main.js
+++ journal_beta/main/static/js/main.js
@@ -2,7 +2,7 @@ function show_side_block(){
     let side_block = document.getElementsByClassName('side_block')[0];
     let info = document.getElementsByClassName('info')[0];
     let side_block_styles = getComputedStyle(side_block);
-    if (side_block_styles.left === '-500px'){
+    if (side_block_styles.left !== '0px'){
         side_block.style.setProperty('left', '0px');
         side_block.style.setProperty('box-shadow', 'rgba(0,0,0,0.16) 0 0 2px');
         info.innerHTML =
@@ -10,7 +10,7 @@ function show_side_block(){
             '    close\n' +
             '</span>'
     } else{
-        side_block.style.setProperty('left', '-500px');
+        side_block.style.setProperty('left', '-5000px');
         side_block.style.setProperty('box-shadow', 'none');
         info.innerHTML =
             '<span class="material-icons-outlined">\n' +
